3326fde90179dbb39257bbe0f9fcf499a63ee350
jankoabel <jankoabel2022@gmail.com>
2024-06-30 08:12:22 -0700

    done

--- Spam Email Classification.py
+++ Spam Email Classification.py
@@ -1,12 +1,6 @@
 
 """
-@author: Kianoush 
-
-GitHUb: https://github.com/Kianoush-h
-YouTube: https://www.youtube.com/channel/UCvf9_53f6n3YjNEA4NxAkJA
-LinkedIn: https://www.linkedin.com/in/kianoush-haratiannejadi/
-
-Email: haratiank2@gmail.com
+@author:Abel Janko
 
 """
 
